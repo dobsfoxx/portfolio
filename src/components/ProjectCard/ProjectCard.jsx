f30b58d426caf0c3cfbe78214af9f89b77dccfac
dobsfoxx <asuosdgx@edu.uniube.br>
2025-11-26 13:45:15 -0300

    fix: responsividade card

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,6 +1,6 @@
 export default function ProjectCard({ title, description, url, image }) {
   return (
-    <article className="card p-6 bg-white border rounded-lg flex flex-col items-center">
+    <article className="card p-6 h-[400px] bg-white border rounded-lg flex flex-col items-center">
       <h3 className="text-xl font-semibold">{title}</h3>
       <img src={image} alt={title} className="max-h-[200px] object-contain mt-2" />
       <p className="mt-2 text-sm text-gray-600">{description}</p>
